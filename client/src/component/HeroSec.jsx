724cc1dc623c34bad31e65ce6a83a31487463038
Shivam040903 <skv040903@gmail.com>
2025-07-14 13:32:35 +0530

    created login form, made changes in nav, and a env file created

--- client/src/component/HeroSec.jsx
+++ client/src/component/HeroSec.jsx
@@ -8,7 +8,7 @@ const HeroSec = () => {
         <p className="bg-[#49B9FF]/50 px-3.5 py-1 rounded-full mt-20">
           The ultimate Hotel Experience
         </p>
-        <h1 className="font-playfair text-2x1 md:text-5x1 md:text-[56px] md:leading-[56px] font-bold md:font-extrabold max-w-x1 mt-4">
+        <h1 className="font-playfair text-2xl md:text-5xl md:text-[56px] md:leading-[56px] font-bold md:font-extrabold max-w-xl mt-4">
           Discover Your Perfect Gateway Destination
         </h1>
         <p className="max-w-130 mt-2 text-sm md:text-base">
