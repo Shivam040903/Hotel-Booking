5f86d87eaa7315adde5e7e3d7f81b29b028c1c2c
Shivam040903 <skv040903@gmail.com>
2025-07-19 12:19:39 +0530

    Made the owner oage with navbar and sidebar  and completed the Dashboard section.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,11 @@ import Footer from './component/Footer'
 import AllRooms from './pages/AllRooms'
 import RoomDetails from './pages/RoomDetails'
 import MyBookings from './pages/MyBookings'
+import HotelReg from './component/HotelReg'
+import Layout from './pages/hotelOwner/Layout'
+import Dashboard from './pages/hotelOwner/Dashboard'
+import AddRoom from './pages/hotelOwner/AddRoom'
+import ListRoom from './pages/hotelOwner/ListRoom'
 
 
 const App = () => {
@@ -15,12 +20,18 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath &&<Nav/>}
+      {false && <HotelReg/>}
       <div className='min-h-[70vh]'>
        <Routes>
         <Route path='/' element={<Homepage/>} />
         <Route path='/rooms' element={<AllRooms/>} />
         <Route path='/rooms/:id' element={<RoomDetails/>} />
         <Route path='/my-bookings' element={<MyBookings/>} />
+        <Route path='/owner' element={<Layout/>}>
+          <Route index element={<Dashboard/>}/>
+          <Route path="add-room" element={<AddRoom/>}/>
+          <Route path="list-room" element={<ListRoom/>}/>
+        </Route>
        </Routes>
        <Footer/>
       </div>
